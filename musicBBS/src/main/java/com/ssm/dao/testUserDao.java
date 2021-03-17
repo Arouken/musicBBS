cee70300b537e8d19941b7b5349200cd87735ec7
zhou <1966686263@qq.com>
2021-03-17 18:54:05 +0800

    创建项目

---
+++ musicBBS/src/main/java/com/ssm/dao/testUserDao.java
@@ -0,0 +1,13 @@
+package com.ssm.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.ssm.pojo.testUser;
+
+public interface testUserDao {
+	
+	@Select("select * from user_info where userName = #{userName} and password = #{password}")
+	public testUser findUserInfoByCond(@Param("userName") String userName, @Param("password") String password);
+
+}
---
+++ musicBBS/src/main/java/com/ssm/pojo/testUser.java
@@ -0,0 +1,39 @@
+package com.ssm.pojo;
+
+public class testUser {
+	private int id;
+	private String userName;
+	private String password;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
